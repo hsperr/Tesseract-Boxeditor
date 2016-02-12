7390901d6c4fb55be3ae96d4be314127127eeb6c
hsperr <henning.sperr@gmail.com>
2016-02-12 16:59:38 +0900

    ENH: cleanup code, move globals to parameters FIX: reading and saving boxfiles is consistent

--- run_box_editor.py
+++ run_box_editor.py
@@ -6,19 +6,24 @@ import sys
 import os
 import glob
 
+def print_next_letter(letters, rectangles):
+    if len(rectangles)<len(letters):
+        print("Draw a box around:",letters[len(rectangles)], [letters[len(rectangles)]])
+    else:
+        print("Seems we are finished, press 's' to save and 'n' for next image")
+
+def drawRectangles(img, rectangles):
+    for cnt, (x1, y1, x2, y2) in enumerate(rectangles):
+        cv2.rectangle(img, (x1, y1), (x2, y2), BLUE, 1)
+        #cv2.putText(img, str(cnt), (x2, y2+2), cv2.FONT_HERSHEY_SIMPLEX, 1, 1)
+
 BLUE = [255,0,0]        # rectangle color
 ix, iy = -1, -1
 rectangles = []
 letters = []
 
-def drawRectangles():
-    for cnt, (x1, y1, x2, y2) in enumerate(rectangles):
-        cv2.rectangle(img, (x1, y1), (x2, y2), BLUE, 1)
-        cv2.putText(img, str(cnt), (x2, y2+2), cv2.FONT_HERSHEY_SIMPLEX, 1, 1)
-        #:TODO : Write number under each rectangle (for easier typing later)
-
 def onmouse(event, x, y, flags, param):
-    global rectangle, ix, iy, img, img2, buffer_img
+    global rectangles, ix, iy, img, letters
     # Draw Rectangle
     y=y+3
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -28,20 +33,12 @@ def onmouse(event, x, y, flags, param):
             ix,iy = x,y
         else:
             rectangles.append((min(ix, x), min(iy, y), max(ix, x), max(iy,y)))
-            img = img2.copy()
-            drawRectangles()
-            #buffer_img = img.copy()
-            print(rectangles)
-            ix, iy = -1, -1
+            drawRectangles(img, rectangles)
             cv2.imshow('input',img)
-    elif event == cv2.EVENT_MOUSEMOVE:
-        pass
-    elif event == cv2.EVENT_LBUTTONUP:
-        pass
+            print_next_letter(letters, rectangles)
+            ix, iy = -1, -1
 
-
-def load_box(filename):
-    box_filename = filename[:-4] + '.box'
+def load_box(box_filename):
     print(box_filename)
     rectangles = []
     if os.path.exists(box_filename):
@@ -49,19 +46,18 @@ def load_box(filename):
             with open(box_filename) as f:
                 split = f.read().split('\n')
                 print(split)
-                rectangles = [map(int, x.split(' ')[1:-1]) for x in split[:-1]]
+                rectangles = [map(int, x.split(' ')) for x in split[:-1]]
         except Exception as e:
             print("Couldn't load boxfile: {}".format(e))
     return rectangles
 
-def load_letters(filename):
-    letter_filename = filename[:-4] + '.txt'
+def load_letters(letter_filename):
     print(letter_filename)
     rectangles = []
     if os.path.exists(letter_filename):
         try:
             with open(letter_filename) as f:
-                split = f.read().decode('utf-8').replace('\n', '').replace(' ', '').replace(u'\u3000', '')
+                split = f.read().decode('utf-8').replace('\n', '').replace(' ', '').replace(u'\u3000', '').replace('\t', '')
                 print(split)
                 letters = [x for x in split if not x == u'\u3000']
                 print(letters)
@@ -69,67 +65,69 @@ def load_letters(filename):
             print("Couldn't load letters: {}".format(e))
     return letters
 
-def save_box():
-    with open(box_filename, 'w') as f:
+def save_boxes(filename, rectangles):
+    with open(filename, 'w') as f:
       for vector in rectangles:
-          f.write(','.join([str(x) for x in vector])+'\n')
-
-def undo_rectangle():
-    global img, img2, rectangles
-    rectangles = rectangles[:-1]
-    img = img2.copy()
-    drawRectangles()
-
-def reset():
-    global img, img2, rectangles
-    print("resetting \n")
-    img = img2.copy()
-    rectangles = []
-    drawRectangles()
+          f.write(' '.join([str(x) for x in vector])+'\n')
 
 def load_image(filename):
-    rectangles = load_box(filename)
-    letters = load_letters(filename)
     img = cv2.imread(filename)
-    original_img = img.copy()                               # a copy of original image
-    drawRectangles()
-    return img, original_img, rectangles, letters
+    return img
 
+def read_filenames_from_folder(folder):
+    images = list(glob.glob(folder+"*.jpg"))
+    images.extend(list(glob.glob(folder+"*.png")))
+    return images
+
+def create_window_and_register_callback():
+    cv2.namedWindow('input')
+    cv2.setMouseCallback('input',onmouse)
 
 if __name__ == '__main__':
     global img, img2, rectangles, letters
+
     # Loading images
     if len(sys.argv) == 2:
         folder = sys.argv[1] # for drawing purposes
     else:
         folder = './'
 
-    images = list(glob.glob(folder+"*.jpg"))
-    images.extend(list(glob.glob(folder+"*.png")))
+    if not os.path.isdir(folder):
+        print("Please secify a folder as first argument, see README.md for details.")
+
+    image_names = read_filenames_from_folder(folder)
 
     current_image = 0
-    filename = images[current_image]
-    img, img2, rectangles, letters = load_image(filename)
-    print(letters)
+    filename = image_names[current_image]
+    box_filename = filename[:-4]+'.box'
+    letter_filename = filename[:-4]+'.txt'
 
-    cv2.namedWindow('input')
-    cv2.setMouseCallback('input',onmouse)
+    img = load_image(filename)
+    original = load_image(filename)
+    empty_image = np.matrix(img.shape)
+
+    rectangles = load_box(box_filename)
+    letters = load_letters(letter_filename)
+
+    create_window_and_register_callback()
 
     while(1):
         print("Pls Input")
-        cv2.imshow('input',img)
-        print("Next letter:",letters[len(rectangles)], [letters[len(rectangles)]])
+        drawRectangles(img, rectangles)
+        cv2.imshow('input', img)
+        print_next_letter(letters, rectangles)
         k = 0xFF & cv2.waitKey(0)
-
         # key bindings
         if k == 27:         # esc to exit
             break
         elif k == ord('s'): # save rectangles
-            save_box()
+            save_boxes(box_filename, rectangles)
         elif k == ord('u'):
-            undo_rectangle()
+            rectangles = rectangles[:-1]
+            img = original.copy()
         elif k == ord('r'): # reset everything
-            reset()
+            rectangles = []
+            img = original.copy()
         elif k == ord('n'):
             current_image+=1
             if current_image>=len(images):
